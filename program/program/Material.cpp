96ac1fb3b757ca57bab560e230c49e20cd1167b9
lollonoa <tlgja0221@gmail.com>
2025-09-25 19:47:22 +0900

    2025.09.25.

---
+++ program/program/Material.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Material.h"
+
+Material::Material()
+{
+	cout << "material이 생성되었습니다." << endl;
+}
+
+Material::~Material()
+{
+	cout << "material이 소멸되었습니다." << endl;
+}
---
+++ program/program/Resource.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Resource.h"
+
+Resource::Resource()
+{
+	cout << "Resource이 생성되었습니다." << endl;
+}
+
+Resource::~Resource()
+{
+	cout << "Resource이 소멸되었습니다." << endl;
+}
